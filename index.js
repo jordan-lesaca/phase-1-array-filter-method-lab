76533454a48e32d489efa3b1fbfdd89e508c14c6
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-06-20 10:58:49 -0700

    commit tree 310157c260163df1088a43a0ca34ca9a115bb34f

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching (arrayDrivers, name) {
+    return arrayDrivers.filter( driver => driver.toUpperCase() == name.toUpperCase())
+}
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter(driverName => {
+        let match = name.length
+        return driverName.slice(0, match) === name
+    })
+}   
+
+function matchName(drivers, name){
+    return drivers.filter(driver => driver.name === name)
+}  
